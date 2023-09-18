7e16f778931c585edc6814a86f9d62c54dbdda23
WenYHsieh <menlen6205@gmail.com>
2023-09-18 15:54:03 +0800

    fix deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,23 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Chart from './components/Chart.tsx';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/:year/:country/:town',
+          element: <Chart />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/:year/:country/:town',
-        element: <Chart />,
-      },
-    ],
+    basename: '/BD-assignment',
   },
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
