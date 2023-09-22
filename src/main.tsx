19babe852509117803ff435ef200eaad8795f694
WenYHsieh <menlen6205@gmail.com>
2023-09-22 16:01:50 +0800

    modify react router back to hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Chart from './components/Chart.tsx';
 
-// browser router
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          path: '/:year/:country/:town',
-          element: <Chart />,
-        },
-      ],
-    },
-  ],
-  { basename: '/BD-assignment' },
-);
-
-// hash router
-// const router = createHashRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     children: [
-//       {
-//         path: '/:year/:country/:town',
-//         element: <Chart />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/:year/:country/:town',
+        element: <Chart />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
