41a6f31bb4239052dfced26a9d0d1aeb6b854306
WenYHsieh <menlen6205@gmail.com>
2023-09-22 15:42:45 +0800

    test browserRouter solution

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Chart from './components/Chart.tsx';
 
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/:year/:country/:town',
-        element: <Chart />,
-      },
-    ],
-  },
-]);
+// browser router
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/:year/:country/:town',
+          element: <Chart />,
+        },
+      ],
+    },
+  ],
+  { basename: '/BD-assignment' },
+);
+
+// hash router
+// const router = createHashRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/:year/:country/:town',
+//         element: <Chart />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
